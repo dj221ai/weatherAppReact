d74f90f14e4f2a14156e9b43ebe17b4e169ba895
Ved Jha <vedprakash@fanclash.in>
2023-07-29 19:13:58 +0530

    searchbar updated

--- src/components/Search/Searchbar.js
+++ src/components/Search/Searchbar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from '../UI/Button'
 import './Searchbar.css';
 
 
 const Searchbar = (props) => {
+
+    const [city, setCity] = useState("");
+
+    const cityHandler = (event) => {
+        console.log(event.target.value);
+        setCity(event.target.value)
+    }
+    const submitHandler = (event) => {
+        console.log("city is >>>> ", city);
+    }
+
+
     return (
         <>
             <div className="search">
-                <input type="text" className="searchTerm" placeholder="Enter Your City"/>
+                <input type="text" className="searchTerm" placeholder="Enter Your City" value={city} onChange={cityHandler}/>
             </div>
+                <Button type="submit" onClick={submitHandler}>Search</Button>
         </>
     )
 }
