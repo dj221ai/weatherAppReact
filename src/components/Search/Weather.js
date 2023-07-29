a5b9b83c2229abcf54218596f6a88ddde3fa9c85
Ved Jha <vedprakash@fanclash.in>
2023-07-29 18:52:03 +0530

    card updated

--- src/components/Search/Weather.js
+++ src/components/Search/Weather.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Button from "../UI/Button";
 import Searchbar from "./Searchbar";
+import Cards from "../UI/Cards";
 
 const Weather = (props) => {
     return (
         <>
             <Searchbar />
             <Button type="submit">Search</Button>
+            <Cards>
+                Temp 30
+            </Cards>
         </>
     )
 }
