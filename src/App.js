d9bf7c3b10f344c5f8b11c0bddab347bd6211b45
Ved Jha <vedprakash@fanclash.in>
2023-07-31 23:35:30 +0530

    working useeffect

--- src/App.js
+++ src/App.js
@@ -9,33 +9,37 @@ function App() {
 
   const [inputCity, setInputCity] = useState("");
   // const [airQuality, setAirQuality] = useState("no");
-  const [data, setData] = useState({});
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const url = `${process.env.REACT_APP_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${inputCity}`
+        console.log("url >>> ", url);
+        const response = await fetch(url);
+        console.log(response);
+        const resJson = await response.json();
+        setData(resJson);
+        // await fetch(url)
+        // .then(res => res.json())
+        // .then(result => {
+        //   console.log(result);
+        //   setData(result)
+        // });
+    };
+
+    fetchData();
+  }, [inputCity])
+
+
 
   const getCityData = (inputCity) => {
     console.log("input city is >>> ", inputCity);
-    // setAirQuality(airQuality);
     setInputCity(inputCity);
   }
   console.log("input city >>> ", inputCity)
+  console.log("data >>> ", data);
   
-  // const getWeatherData = (cityName) => {
-  //   console.log(process.env.REACT_APP_API_URL, "------", process.env.REACT_APP_API_KEY);
-  //   fetch(
-  //     `${process.env.REACT_APP_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${cityName}`
-  //     // `${process.env.REACT_APP_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${inputCity}&aqi=${airQuality}`
-  //   )
-  //   .then(results => results.json())
-  //   .then(jsondata => {
-  //     console.log(jsondata);
-  //     setData(jsondata)
-  //   })
-  //   .catch(err => console.log("errr is ", err));
-  // }
-
-
-  // useEffect(() => {
-  //   // getWeatherData()
-  // })
+  
 
   return (
     <>
